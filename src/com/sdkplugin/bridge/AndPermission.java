16e74c7e5c8a552775c48a822dd7f1d76056a906
canyon <gongyanghui1986@163.com>
2019-07-13 17:14:26 +0800

    动态权限申请 6.0 - target 26 以后

---
+++ src/com/sdkplugin/bridge/AndPermission.java
@@ -0,0 +1,59 @@
+package com.sdkplugin.bridge;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+
+/**
+ * 类名 : Android 6.0 以后权限申请 <br/>
+ * 作者 : canyon / 龚阳辉 <br/>
+ * 时间 : 2019-07-13 13：30 <br/>
+ * 功能 : 在继承UnityPlayerActivity里面调用
+ */
+public class AndPermission {
+	static final String[] perm = new String[] { Manifest.permission.READ_PHONE_STATE, Manifest.permission.WRITE_EXTERNAL_STORAGE };
+
+	static int reqCode = 2; // 10000
+
+	// 声明一个集合，在后面的代码中用来存储用户拒绝授权的权
+	static public void initPermissions(Activity activity) {
+		List<String> _list = new ArrayList<>();
+		for (int i = 0; i < perm.length; i++) {
+			if (ContextCompat.checkSelfPermission(activity, perm[i]) != PackageManager.PERMISSION_GRANTED) {
+				_list.add(perm[i]);
+			}
+		}
+		if (!_list.isEmpty()) {
+			String[] _arrs = {};
+			_arrs = _list.toArray(_arrs);// 将List转为数组
+			ActivityCompat.requestPermissions(activity, _arrs, reqCode);
+		}
+	}
+
+	static public void initPermissions() {
+		initPermissions(AndU3DBasic.getCurActivity());
+	}
+
+	static public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults, Activity activity) {
+		if (reqCode == requestCode) {
+			for (int i = 0; i < grantResults.length; i++) {
+				if (grantResults[i] != PackageManager.PERMISSION_GRANTED) {
+					// 判断是否勾选禁止后不再询问
+					boolean showRequestPermission = ActivityCompat.shouldShowRequestPermissionRationale(activity, permissions[i]);
+					if (showRequestPermission) {
+						// showToast("权限未申请");
+					}
+				}
+			}
+		}
+	}
+
+	static public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+		onRequestPermissionsResult(requestCode, permissions, grantResults, AndU3DBasic.getCurActivity());
+	}
+}
