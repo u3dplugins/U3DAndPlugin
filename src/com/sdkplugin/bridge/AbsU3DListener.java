dfb3defe2cc90130bf29a4cd1ec3e8faa97178b5
canyon <gongyanghui1986@163.com>
2019-07-15 10:07:22 +0800

    加请求，响应打印

--- src/com/sdkplugin/bridge/AbsU3DListener.java
+++ src/com/sdkplugin/bridge/AbsU3DListener.java
@@ -6,13 +6,14 @@ package com.sdkplugin.bridge;
  * 时间 : 2016-05-30 10：30 <br/>
  * 功能 :
  */
-public abstract class AbsU3DListener extends AndU3DTools implements
-		IU3DListener {
+public abstract class AbsU3DListener extends AndU3DTools implements IU3DListener {
 
 	static public final String CODE_SUCCESS = "success";
 	static public final String CODE_WAIT = "wait";
 	static public final String CODE_FAILS = "fails";
 	static public final String CODE_ERROR = "error";
+	static final String fmt_req = "=== %s,request=,msg = [%s]";
+	static final String fmt_res = "=== %s,response=,msg = [%s]";
 
 	protected String ugobjName = "";
 	protected String ugobjMethod = "";
@@ -21,26 +22,35 @@ public abstract class AbsU3DListener extends AndU3DTools implements
 	static final protected int LEV_LOG_NORMAL = 0;
 	static final protected int LEV_LOG_MUST = 5;
 	static final protected int LEV_LOG_NONE = 10;
-	
+
 	protected int logLevel = LEV_LOG_ALL;
 	protected String logHead = "plugin";
 
-	protected void logInfo(String msg) {
-		debugLog(msg, LEV_LOG_NORMAL);
-	}
-	
-	protected void logMust(String msg) {
-		debugLog(msg, LEV_LOG_MUST);
-	}
-
-	protected void debugLog(String msg, int level) {
-		if(logLevel >= LEV_LOG_NONE)
+	protected void debugLog(String msg, int level, boolean isRequest) {
+		if (logLevel >= LEV_LOG_NONE)
 			return;
-		
+
 		if (level < logLevel || msg == null || msg.length() <= 0)
 			return;
 
-		System.out.println(String.format("== %s ==,msg = [%s]", logHead, msg));
+		String _v = String.format(isRequest ? fmt_req : fmt_res, logHead, msg);
+		System.out.println(_v);
+	}
+
+	protected void logInfo(String msg, boolean isRequest) {
+		debugLog(msg, LEV_LOG_NORMAL, isRequest);
+	}
+
+	protected void logInfo(String msg) {
+		logInfo(msg, true);
+	}
+
+	protected void logMust(String msg, boolean isRequest) {
+		debugLog(msg, LEV_LOG_MUST, isRequest);
+	}
+
+	protected void logMust(String msg) {
+		logMust(msg, true);
 	}
 
 	@Override
@@ -51,6 +61,7 @@ public abstract class AbsU3DListener extends AndU3DTools implements
 
 	@Override
 	public void response2Unity(String json) throws Exception {
+		logInfo(json, false);
 		sendMsg(this.ugobjName, this.ugobjMethod, json);
 	}
 
