c851466c828406a474a611a17d05a3a8543f7512
canyon <gongyanghui1986@163.com>
2020-03-19 11:06:00 +0800

    添加权限申请，添加崩溃实现脚本

---
+++ src/com/crash/ImplAndCrash.java
@@ -0,0 +1,91 @@
+package com.crash;
+
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import com.crash.IAndCrash;
+
+public class ImplAndCrash implements IAndCrash {
+	String _url0 = "http://60.205.217.89:9902/statistics", _url = null;
+
+	private ImplAndCrash() {
+	}
+
+	@Override
+	public void sendCrash2Sv(final String info) {
+		if (info == null || info.isEmpty())
+			return;
+
+		Thread _t = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				try {
+					String msg = String.format("cmd=log&name=crash&val=%s", info);
+					byte[] bts = msg.getBytes("UTF-8");
+					URL _url = new URL(getUrl());
+					HttpURLConnection conn = (HttpURLConnection) _url.openConnection();
+					// 设置请求数据类型 - 浏览器编码类型
+					conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+					// 编码格式
+					conn.setRequestProperty("Charset", "UTF-8");
+					// 设置是否使用缓存 - POST不能使用缓存?
+					conn.setUseCaches(false);
+					// 维持长连接 Keep-Alive close
+					conn.setRequestProperty("Connection", "close");
+					// 设置接受所有类型
+					conn.setRequestProperty("Accept-Charset", "UTF-8");
+					conn.setRequestProperty("Accept", "*/*");
+					conn.setRequestProperty("Content-Length", String.valueOf(bts.length));
+					// 请求超时
+					conn.setConnectTimeout(60000);
+					// 读取超时
+					conn.setReadTimeout(60000);
+
+					// 是否设置输入的内容
+					conn.setDoInput(true);
+					// 是否设置输出的内容
+					conn.setDoOutput(true);
+
+					conn.setRequestMethod("POST");
+
+					// 建立实际的连接
+					conn.connect();
+
+					OutputStream outPut = conn.getOutputStream();
+					outPut.write(bts);
+					outPut.flush();
+					outPut.close();
+					conn.getResponseCode();
+					conn.disconnect();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+		_t.start();
+	}
+
+	public void init(String url) {
+		this._url = url;
+	}
+
+	protected String getUrl() {
+		if (this._url == null || this._url.isEmpty())
+			return this._url0;
+		return this._url;
+	}
+
+	public static void main(String[] args) {
+		getInstance().sendCrash2Sv("time:" + System.currentTimeMillis());
+	}
+
+	private static ImplAndCrash _instance = null;
+
+	static final public ImplAndCrash getInstance() {
+		if (_instance == null) {
+			_instance = new ImplAndCrash();
+		}
+		return _instance;
+	}
+}
--- src/com/sdkplugin/bridge/AndPermission.java
+++ src/com/sdkplugin/bridge/AndPermission.java
@@ -20,20 +20,39 @@ public class AndPermission extends AndBasic {
 	// Manifest.permission.READ_PHONE_STATE
 	static int reqCode = 2;
 	static int Code_1 = PackageManager.PERMISSION_GRANTED;
+	static public boolean isReRequest = true;
 
 	// 是否授权
-	static public boolean isGrant(Activity activity, String permission) {
+	static final public boolean isGrant(Activity activity, String permission) {
 		return ContextCompat.checkSelfPermission(activity, permission) == Code_1;
 	}
 
+	// 判断是否勾选禁止后不再询问 (是否可以再显示)
+	static final public boolean isCanShowPermission(Activity activity, String permission) {
+		return ActivityCompat.shouldShowRequestPermissionRationale(activity, permission);
+	}
+
+	// 取得权限状态[0=已申请,1=未申请(可以申请),2=未申请(不能再申请)]
+	static final public int grantState(Activity activity, String permission) {
+		int _ret = 0;
+		boolean isGrant = ContextCompat.checkSelfPermission(activity, permission) == Code_1;
+		if (!isGrant) {
+			boolean isCanShow = isCanShowPermission(activity, permission);
+			_ret = isCanShow ? 1 : 2;
+		}
+		return _ret;
+	}
+
 	// 声明一个集合，在后面的代码中用来存储用户拒绝授权的权
-	static public void initPermissions(Activity activity, String... permissions) {
+	static final public void initPermissions(Activity activity, String... permissions) {
 		if (permissions == null || permissions.length <= 0)
 			return;
 
 		List<String> _list = new ArrayList<>();
+		int _st = -1;
 		for (int i = 0; i < permissions.length; i++) {
-			if (!isGrant(activity, permissions[i])) {
+			_st = grantState(activity, permissions[i]);
+			if (_st == 1) {
 				_list.add(permissions[i]);
 			}
 		}
@@ -44,17 +63,28 @@ public class AndPermission extends AndBasic {
 		}
 	}
 
-	static public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults, Activity activity) {
+	static final public void onRequestPermissionsResult2(int requestCode, String[] permissions, int[] grantResults, Activity activity) {
 		if (reqCode == requestCode) {
 			for (int i = 0; i < grantResults.length; i++) {
 				if (grantResults[i] != Code_1) {
 					// 判断是否勾选禁止后不再询问
-					boolean showRequestPermission = ActivityCompat.shouldShowRequestPermissionRationale(activity, permissions[i]);
-					if (showRequestPermission) {
+					if (isCanShowPermission(activity, permissions[i])) {
 						// showToast("权限未申请");
 					}
 				}
 			}
 		}
+	}
+
+	static final public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults, Activity activity) {
+		if (reqCode == requestCode) {
+			if (isReRequest) {
+				initPermissions(activity, permissions);
+			}
+		}
+	}
+
+	static final public void reqPermission(Activity activity, String permission) {
+		initPermissions(activity, permission);
 	}
 }
