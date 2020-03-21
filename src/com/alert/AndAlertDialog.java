e6af8082a59fcc7531b2db00b84a62ea1e20a210
canyon <gongyanghui1986@163.com>
2020-03-21 14:08:21 +0800

    添加构造函数

--- src/com/alert/AndAlertDialog.java
+++ src/com/alert/AndAlertDialog.java
@@ -14,13 +14,22 @@ import android.support.annotation.Nullable;
 public class AndAlertDialog {
 	private IAndAlert mImpl;
 	private Context mContext;
+	private int nBtnType = 0;
 
 	private AndAlertDialog() {
+	}
+
+	public AndAlertDialog init(@Nullable Context context, IAndAlert impl, int btnType) {
+		this.mImpl = impl;
+		this.mContext = context;
+		this.nBtnType = btnType > 2 ? 0 : btnType;
+		return this;
 	}
 
 	public AndAlertDialog init(@Nullable Context context, IAndAlert impl) {
 		this.mImpl = impl;
 		this.mContext = context;
+		this.nBtnType = 0;
 		return this;
 	}
 
@@ -30,24 +39,45 @@ public class AndAlertDialog {
 		if (btnCancel == null || btnCancel.isEmpty())
 			btnCancel = "取消";
 
-		AlertDialog.Builder alertDialog = new AlertDialog.Builder(context).setTitle(title).setMessage(msg).setPositiveButton(btnSure, new DialogInterface.OnClickListener() {
-			@Override
-			public void onClick(DialogInterface dialog, int which) {
-				dialog.dismiss();
-				excuteCall(true);
-			}
-		}).setNegativeButton(btnCancel, new DialogInterface.OnClickListener() {
-			@Override
-			public void onClick(DialogInterface dialog, int which) {
-				dialog.dismiss();
-				excuteCall(false);
-			}
-		});
+		AlertDialog.Builder alertDialog = new AlertDialog.Builder(context).setTitle(title).setMessage(msg);
+		boolean isBl = this.nBtnType == 0 || this.nBtnType == 1;
+		if (isBl) {
+			alertDialog.setPositiveButton(btnSure, new DialogInterface.OnClickListener() {
+				@Override
+				public void onClick(DialogInterface dialog, int which) {
+					dialog.dismiss();
+					excuteCall(true);
+				}
+			});
+		}
+
+		isBl = this.nBtnType == 0 || this.nBtnType == 2;
+		if (isBl) {
+			alertDialog.setNegativeButton(btnCancel, new DialogInterface.OnClickListener() {
+				@Override
+				public void onClick(DialogInterface dialog, int which) {
+					dialog.dismiss();
+					excuteCall(false);
+				}
+			});
+		}
 		alertDialog.show();
 	}
 
 	public void show(@Nullable String title, @Nullable String msg, String btnSure, String btnCancel) {
 		show(this.mContext, this.mImpl, title, msg, btnSure, btnCancel);
+	}
+
+	public void show(@Nullable String title, @Nullable String msg, String btnSure) {
+		show(this.mContext, this.mImpl, title, msg, btnSure, null);
+	}
+
+	public void show(@Nullable String title, @Nullable String msg) {
+		show(this.mContext, this.mImpl, title, msg, null, null);
+	}
+
+	public void showCancel(@Nullable String title, @Nullable String msg, String btnCancel) {
+		show(this.mContext, this.mImpl, title, msg, null, btnCancel);
 	}
 
 	final public void excuteCall(boolean isSure) {
@@ -56,12 +86,20 @@ public class AndAlertDialog {
 		}
 	}
 
-	private static AndAlertDialog _instance = null;
+	static final public AndAlertDialog build(@Nullable Context context, IAndAlert impl, int btnType) {
+		AndAlertDialog _ret = new AndAlertDialog();
+		return _ret.init(context, impl, btnType);
+	}
 
-	static final public AndAlertDialog getInstance() {
-		if (_instance == null) {
-			_instance = new AndAlertDialog();
-		}
-		return _instance;
+	static final public AndAlertDialog build(@Nullable Context context, IAndAlert impl) {
+		return build(context, impl, 0);
+	}
+
+	static final public AndAlertDialog buildOne(@Nullable Context context, IAndAlert impl, boolean isUseSure) {
+		return build(context, impl, isUseSure ? 1 : 2);
+	}
+
+	static final public AndAlertDialog buildOne(@Nullable Context context, IAndAlert impl) {
+		return buildOne(context, impl, true);
 	}
 }
